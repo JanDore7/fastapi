8a3f1d9d6519f635420a7e5b5449bbaf11d06500
JanDore7 <jan_dore2030@proton.me>
2024-09-24 06:02:31 +0300

    Добавил description

--- fapi/hotels.py
+++ fapi/hotels.py
@@ -1,4 +1,4 @@
-from fastapi import  Query, Body, APIRouter
+from fastapi import  Query, Body, APIRouter, Path
 
 hotels = [
     {"id": 1, "title": "Sochi", "name": "Сочи"},
@@ -9,23 +9,23 @@ hotels = [
 router = APIRouter(prefix='/hotels', tags=["Отели"])
 
 
-@router.get("", description="Получить список отелей или отель по названию")
+@router.get("", description="Получить список отелей или отель по названию", name="Список отелей")
 async def get_hotels(name: str | None = Query(None, description="Название отеля" )):
     if name in [hotel["name"] for hotel in hotels]:
         return next(hotel for hotel in hotels if hotel["name"] == name)
     return hotels
 
 
-@router.delete("/{hotel_name}", description="Удалить отель по названию")
-async def delete_hotel(hotel_name: str):
+@router.delete("/{hotel_name}", description="Удалить отель по названию", name="Удалить отель")
+async def delete_hotel(hotel_name: str = Path(...,description="Название отеля")):
     global hotels
     print(hotel_name)
     hotels = [hotel for hotel in hotels if hotel["name"] != hotel_name]
     return {"status": "ok"}
 
 
-@router.post("", description="Создать отель")
-async def create_hotel(hotel: dict = Body()):
+@router.post("", description="Создать отель", name="Создать отель")
+async def create_hotel(hotel: dict = Body(..., example={"title": "Sochi", "name": "Сочи"}, description="Данные отеля")):
     global hotels
     hotels.append(
         {
@@ -37,8 +37,9 @@ async def create_hotel(hotel: dict = Body()):
     return {"status": "ok"}
 
 
-@router.put("/{hotel_id}", description="Обновить отель")
-async def update_hotel(hotel_id: int, hotel: dict = Body(..., example={"title": "New York", "name": "Нью-Йорк"})):
+@router.put("/{hotel_id}", description="Обновить отель", name="Обновить отель")
+async def update_hotel(hotel_id: int = Path(..., description="Идентификатор гостиницы"), hotel: dict = Body(..., example={"title": "New York", "name": "Нью-Йорк"},
+                                                         description="Новые данные отеля")):
     global hotels
     hotels = [hotel for hotel in hotels if hotel["id"] != hotel_id]
     hotels.append(
@@ -51,8 +52,10 @@ async def update_hotel(hotel_id: int, hotel: dict = Body(..., example={"title": 
     return {"status": "ok", "hotel": hotel}
 
 
-@router.patch("/{hotel_id}", description="Обновить часть информации об отеле")
-async def patch_hotel(hotel_id: int, data_hotel: dict = Body(..., description="Новые данные отеля")):
+@router.patch("/{hotel_id}", description="Обновить часть информации об отеле", name="Редактировать отель")
+async def patch_hotel(
+        hotel_id: int = Path(..., description="Идентификатор гостиницы"),
+        data_hotel: dict = Body(dict, description="Новые данные отеля")):
     global hotels
     if len(data_hotel) < 2:
         for hotel in hotels:

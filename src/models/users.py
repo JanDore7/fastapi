2a6101ad47ca1a395b7eb84357bc71a86264c8d1
JanDore7 <jan_dore2030@proton.me>
2024-10-15 02:53:38 +0300

    Модель UsersOrm

---
+++ src/models/users.py
@@ -0,0 +1,13 @@
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, BIGINT
+from src.database import Base
+
+
+class UsersOrm(Base):
+    __tablename__ = 'users'
+
+    id : Mapped[int] = mapped_column(BIGINT, primary_key=True)
+    email : Mapped[str] = mapped_column(String(200))
+    password : Mapped[str] = mapped_column(String(200))
+
+

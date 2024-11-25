6553618da5872209f51bf8487697ed2503f9a76a
JanDore7 <jan_dore2030@proton.me>
2024-11-26 02:59:27 +0300

    Фоновая задача с fastapi и бесконечным циклом по расписанию.

--- src/api/images.py
+++ src/api/images.py
@@ -7,9 +7,8 @@ router = APIRouter(prefix="/images", tags=["Изображения отелей"
 
 @router.post("")
 def upload_image(file: UploadFile):
+    image_path = f"src/static/images/{file.filename}"
     with open(f"src/static/images/{file.filename}", "wb+") as new_file:
         shutil.copyfileobj(file.file, new_file)
         test_task.delay()
-        resize_image.delay(
-            "/home/dideb/my_utilities/my_projects/fastapi/src/static/images/Без имени.jpeg"
-        )
+        resize_image.delay(image_path)
--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+import asyncio
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
@@ -11,6 +12,8 @@ from fastapi_cache.backends.redis import RedisBackend
 import sys
 from pathlib import Path
 
+from src.api.dependencies import get_db
+
 sys.path.append(str(Path(__file__).parent.parent))
 from src.init import redis_manager
 
@@ -22,8 +25,23 @@ from src.api.facilities import router as router_facilities
 from src.api.images import router as router_images
 
 
+async def send_emails_bookings_today_checkin():
+    async for db in get_db():
+        bookings = await db.get_bookings_with_today_checkin()
+        print(f'{bookings=}')
+
+
+async def run_send_emails_regularly():
+    while True:
+        await send_emails_bookings_today_checkin()
+        await asyncio.sleep(5)
+
+
+
 @asynccontextmanager
 async def lifespan(app: FastAPI):
+    # бесконечный цикл, запускается без await
+    asyncio.create_task(run_send_emails_regularly())
     # Выполняется при старте приложения
     await redis_manager.connect()
     FastAPICache.init(RedisBackend(redis_manager.redis), prefix="fastapi-cache")

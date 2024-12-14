e0d42c36d7cb4abb566a0502fea489a0afc55992
JanDore7 <jan_dore2030@proton.me>
2024-12-14 23:43:33 +0300

    Задача №24

--- tests/integration_tests/bookings/test_api.py
+++ tests/integration_tests/bookings/test_api.py
@@ -1,5 +1,8 @@
 import pytest
 
+from src.database import async_session_null_pool
+from src.utils.db_manager import DBManager
+
 
 @pytest.mark.parametrize(
     "room_id, date_from, date_to, status_code",
@@ -12,6 +15,7 @@ import pytest
         (1, "2025-08-01", "2025-08-10", 500),
         (1, "2025-09-01", "2025-09-10", 200),
     ],
+    # Даем название тесту
     ids=[
         "Valid case 1",
         "Valid case 2",
@@ -41,3 +45,35 @@ async def test_add_booking(
         assert isinstance(res, dict)
         assert res["status"] == "OK"
         assert "data" in res
+
+
+@pytest.fixture(scope="session")
+async def delete_all_bookings():
+    async with DBManager(session_factory=async_session_null_pool) as db_:
+        await db_.bookings.delete()
+        await db_.commit()
+
+
+@pytest.mark.parametrize(
+    "room_id, date_from, date_to, amount",
+    [
+        (1, "2024-08-01", "2024-08-10", 1),
+        (1, "2024-08-02", "2024-08-11", 2),
+        (1, "2024-08-01", "2024-08-05", 3),
+    ],
+)
+async def test_add_and_get_bookings(
+    room_id, date_from, date_to, amount, db, authenticated_ac, delete_all_bookings
+):
+    response = await authenticated_ac.post(
+        "/bookings",
+        json={
+            "room_id": room_id,
+            "date_from": date_from,
+            "date_to": date_to,
+        },
+    )
+    res = response.json()
+    assert isinstance(res, dict)
+    assert res["status"] == "OK"
+    assert "data" in res
